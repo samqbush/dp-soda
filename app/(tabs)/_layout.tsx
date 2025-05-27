de983ea676507b33c480c0032dafbdfb7109b33f
Sam Quakenbush <samqbush@github.com>
2025-05-27 17:15:02 -0600

    feat: Update app name and enhance color scheme in app.json; replace and remove unused images

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,7 +34,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="settings"
         options={{
           title: 'Settings',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="gear" color={color} />,
